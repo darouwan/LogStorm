93420b4b764fb10486fd84bc17821bea45b6a6ec
Junfeng Chen <k-2feng@hotmail.com>
2014-12-24 17:27:18 +0800

    Trident测试， 聚合测试

--- src/main/java/com/wiscom/topology/LogTridentTopology.java
+++ src/main/java/com/wiscom/topology/LogTridentTopology.java
@@ -1,52 +1,70 @@
 package com.wiscom.topology;
 
 import backtype.storm.tuple.Fields;
-import com.wiscom.spout.ReadLogSpout;
-import com.wiscom.spout.TridentTestSpout;
+import backtype.storm.tuple.Values;
 import storm.trident.TridentState;
 import storm.trident.TridentTopology;
+import storm.trident.operation.CombinerAggregator;
 import storm.trident.operation.builtin.Count;
-import storm.trident.operation.builtin.FilterNull;
-import storm.trident.operation.builtin.MapGet;
-import storm.trident.operation.builtin.Sum;
+import storm.trident.testing.FixedBatchSpout;
 import storm.trident.testing.MemoryMapState;
 import storm.trident.testing.Split;
+import storm.trident.tuple.TridentTuple;
 
 /**
  * Created by cjf on 2014/12/22.
  */
 public class LogTridentTopology {
-    public static void main(String[] args) {
+    public static void aggregateTest() {
+        FixedBatchSpout spout = new FixedBatchSpout(new Fields("sentence"), 3,
+                new Values("the cow jumped over the moon"),
+                new Values("the man went to the store and bought some candy"),
+                new Values("four score and seven years ago"),
+                new Values("how many apples can you eat"));
+
+        spout.setCycle(true);
         TridentTopology topology = new TridentTopology();
         TridentState wordCounts =
-                topology.newStream("spout1", new TridentTestSpout())
-                        .each(new Fields("sentence"), new Split(), new Fields("word"))
-                        .groupBy(new Fields("word"))
-                        .persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"))
-                        .parallelismHint(6);
+                topology.newStream("spout1", spout)
+                        .each(new Fields("sentence"), new Split(), new Fields("word")).groupBy(new Fields("word")).
+                        aggregate(new One(), new Fields("one")).
+                        persistentAggregate(new MemoryMapState.Factory(), new Count(), new Fields("count"));
 
-        topology.newDRPCStream("words")
-                .each(new Fields("args"), new Split(), new Fields("word"))
-                .groupBy(new Fields("word"))
-                .stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
-                .each(new Fields("count"), new FilterNull())
-                .aggregate(new Fields("count"), new Sum(), new Fields("sum"));
+        // topology.newDRPCStream("reach").stateQuery(wordCounts,new Fields("word"),new MapGet(),new Fields("count")).aggregate(new One(), new Fields("one"));
+    }
 
-        TridentState urlToTweeters =
-                topology.newStaticState(getUrlToTweetersState());
-        TridentState tweetersToFollowers =
-                topology.newStaticState(getTweeterToFollowersState());
 
-        topology.newDRPCStream("reach")
-                .stateQuery(urlToTweeters, new Fields("args"), new MapGet(), new Fields("tweeters"))
-                .each(new Fields("tweeters"), new ExpandList(), new Fields("tweeter"))
-                .shuffle()
-                .stateQuery(tweetersToFollowers, new Fields("tweeter"), new MapGet(), new Fields("followers"))
-                .parallelismHint(200)
-                .each(new Fields("followers"), new ExpandList(), new Fields("follower"))
-                .groupBy(new Fields("follower"))
-                .aggregate(new One(), new Fields("one"))
-                .parallelismHint(20)
-                .aggregate(new Count(), new Fields("reach"));
+    public static void main(String[] args) {
+        TridentTopology topology = new TridentTopology();
+//        TridentState wordCounts =
+//                topology.newStream("spout1", new TridentTestSpout())
+//                        .each(new Fields("sentence"), new Split(), new Fields("word"))
+//                        D
+//        topology.newDRPCStream("words")
+//                .each(new Fields("args"), new Split(), new Fields("word"))
+//                .groupBy(new Fields("word"))
+//                .stateQuery(wordCounts, new Fields("word"), new MapGet(), new Fields("count"))
+//                .each(new Fields("count"), new FilterNull())
+//                .aggregate(new Fields("count"), new Sum(), new Fields("sum"));
+
+        aggregateTest();
+    }
+
+    private static class One implements CombinerAggregator<Integer> {
+
+        @Override
+        public Integer init(TridentTuple tuple) {
+            return 1;
+        }
+
+        @Override
+        public Integer combine(Integer val1, Integer val2) {
+            return 1;
+        }
+
+        @Override
+        public Integer zero() {
+            return 1;
+        }
     }
 }
